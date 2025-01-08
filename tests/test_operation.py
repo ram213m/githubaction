de50e788d7a5c6b610efa8de7f68777acb9c9761
ram213m <ram.barca@hotmail.com>
2025-01-08 10:07:13 +0530

    Unit Test Cases Commited

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+    assert add(-1,-1) == -2
+
+def test_sub():
+    assert sub(5,0) == 5
+    assert sub(-1,1) == -2
+    assert sub(-1,-1) == 0
